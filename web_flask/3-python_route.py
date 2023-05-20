413b543095a575a4b4d98edfcf1ecbaaba4de577
Hosea Tonzai Favour <hoseafavour123@gmail.com>
2023-05-20 08:53:18 +0000

    Updated 3-python_route

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -23,7 +23,7 @@ def c_is_fun(text):
 
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def python_is_cool(text='is fun'):
+def python_is_cool(text='is cool'):
     return "Python {}".format(text.replace('_', ' '))
 
 
